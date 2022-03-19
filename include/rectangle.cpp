27d563145b829563cca0d27816e519fc51377685
GhostlyCoding <pokeball.181818@gmail.com>
2022-03-19 14:00:09 -0500

    modified rectangle class

--- include/rectangle.cpp
+++ include/rectangle.cpp
@@ -10,9 +10,9 @@ Rectangle::Rectangle(glm::vec3 pos, float width, float height, glm::vec3 rot,
     rotation = glm::translate(rotation, -pos);
     glm::vec4 p2 = glm::vec4(pos.x+width, pos.y, pos.z,1);
     p2 = rotation * p2;
-    glm::vec4 p3 = glm::vec4(pos.x, pos.y+height, pos.z,1);
+    glm::vec4 p3 = glm::vec4(pos.x, pos.y-height, pos.z,1);
     p3 = rotation * p3;
-    glm::vec4 p4 = glm::vec4(pos.x+width, pos.y+height, pos.z,1);
+    glm::vec4 p4 = glm::vec4(pos.x+width, pos.y-height, pos.z,1);
     p4 = rotation * p4;
     Handler::getInstance().registerTriangle(p1,p2,p3,c1,c2,c3);
     Handler::getInstance().registerTriangle(p2,p4,p3,c2,c4,c3);
